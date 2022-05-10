6b99fe8a23625b320f6774bc3a834700382c3102
IKEH1024 <pondau1024@gmail.com>
2022-05-10 15:00:38 +0900

    transferの実装

--- SwiftZoomin8/BankAccount.swift
+++ SwiftZoomin8/BankAccount.swift
@@ -2,8 +2,11 @@ import Foundation
 
 // 競合を起こさないversion
 actor BankAccount {
-    var balance: Int = 0  // 預金残高
+    private(set) var balance: Int = 0  // 預金残高. private(set)で外から参照可能になる
+    
+    @discardableResult
     func deposit(_ amount: Int) -> Int {  // 入金
+        precondition(amount >= 0)  // 起こり得ない。ハンドリング不要なのでクラッシュさせてOK
         balance += amount
         return balance
     }
@@ -19,17 +22,58 @@ actor BankAccount {
         deposit(Int(Double(balance) * rate))
     }
     
+    // MARK: - Transfer
+    
+    struct WithdrawalError: Error {}
+    
+    @discardableResult
+    func withdraw(_ amount: Int) throws -> Int {
+        precondition(amount >= 0)
+        guard balance >= amount else {
+            throw WithdrawalError()
+        }
+        balance -= amount
+        return balance
+    }
+    
+    func transfer(_ amount: Int, to account: BankAccount) async throws {
+        _ = try withdraw(amount)
+        _ = await account.deposit(amount)
+    }
+    
 }
 
 func bankAccountMain() {
 
-    let account: BankAccount = .init()
+    let account1: BankAccount = .init()
+    let account2: BankAccount = .init()
 
     Task {
         // Task内では順番は保証されている
-        _ = await account.deposit(100)  // 100
-        print(await account.deposit(100))  // 200
-        print(await account.getInterest(with: 0.05))  // 210
+        await account1.deposit(100)
+        try await account1.transfer(30, to: account2)
+        print(await account1.balance)
+        print(await account2.balance)
+    }
+    
+//    var foo: Foo = .init()
+//    foo.x = -100
+//    do {
+//        print(try foo.y)
+//    } catch {
+//        print(error.localizedDescription)
+//    }
+}
+
+struct Foo {
+    var x: Int = 0
+    
+    // throwsをつけたComputed Propertyを定義できる
+    var y: Double {
+        get throws {
+            if x < 0 { throw NSError() }
+            return Double(x).squareRoot()
+        }
     }
 }
 
